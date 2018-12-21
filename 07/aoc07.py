f103049c58ee91b727c1005ab6d80d9fa9ce79c2
Ales Podolnik <podolnik@ipp.cas.cz>
2018-12-21 11:01:03 +0100

    Day 7

--- 07/aoc07.py
+++ 07/aoc07.py
@@ -18,40 +18,48 @@ steps = sorted(list(steps))
 
 graph = {}
 times = {}
+dependencies = {}
+init_step = '*'
+
 for step in steps:
     graph[step] = []
     times[step] = ord(step) - 4
+    dependencies[step] = []
 
 for edge in edges:
     graph[edge[0]].append(edge[1])
+    dependencies[edge[1]].append(edge[0])
 
-step_order = ''
-step_avail = {}
-
-init_step = '*'
-prev_candidate = init_step
-
-while len(steps) > 0:
-    candidates = []
-    for s in steps:
-        count = 0
-        for n in graph:
-            e = graph[n]
-            count += 1 if s in e else 0
-        if count == 0:
-            candidates.append(s)
-    candidates.sort()
-
-
-    step_avail[prev_candidate] = candidates
-    prev_candidate = candidates[0]
-
-    print(candidates)
-    step_order += prev_candidate
-    steps.remove(prev_candidate)
-    graph.pop(prev_candidate)
+def Task_A(graph):
+    step_order = ''
+    step_avail = {}
     
-print(step_order)
+    prev_candidate = init_step
+    
+    while len(steps) > 0:
+        candidates = []
+        for s in steps:
+            count = 0
+            for n in graph:
+                e = graph[n]
+                count += 1 if s in e else 0
+            if count == 0:
+                candidates.append(s)
+        candidates.sort()
+    
+    
+        step_avail[prev_candidate] = candidates
+        prev_candidate = candidates[0]
+    
+        print(prev_candidate, candidates)
+        step_order += prev_candidate
+        steps.remove(prev_candidate)
+        graph.pop(prev_candidate)
+        
+    print(step_order)
+    return (step_order, step_avail)
+
+step_order, step_avail = Task_A(dict(graph))
 
 av_b = step_avail
 
@@ -61,27 +69,39 @@ workers = ['-'] * w_count;
 counter = [0] * w_count
 candidates = step_avail.pop('*')
 done = set()
+
+def DoneDependencies(done, dependencies, step):
+    step_dep = dependencies[step]
+    done_dep = True
+    for d in step_dep:
+        if not(d in done):
+            done_dep = False
+            break
+    return done_dep
+
 while True:
     for i in range(w_count):
         if workers[i] == '-':
-            if len(candidates) > 0:
-                workers[i] = candidates.pop(0)
-                counter[i] = times[workers[i]]
-#    print(m, workers)
+            for c_i in range(len(candidates)):
+                if DoneDependencies(done, dependencies, candidates[c_i]):
+                    workers[i] = candidates.pop(c_i)
+                    counter[i] = times[workers[i]]
+                    break
+                
+    print('{:04d}'.format(m), ''.join(workers), ''.join(candidates))
     
     done_now = []
     for i in range(w_count):
         if workers[i] != '-':
             counter[i] -= 1
             if counter[i] == 0:
-                print('done', workers[i])
                 done_now.append(workers[i])
                 workers[i] = '-'
                 counter[i] = 0
-#                print(workers, candidates)
     
+    done.update(done_now)
+        
     for d in done_now:
-        done.add(d)
         if d != step_order[-1]:
             candidates_now = step_avail.pop(d)
             for c in candidates_now:
@@ -92,6 +112,7 @@ while True:
     if len(done) == len(times):
         break
 
+print('{:03d}'.format(m), ''.join(workers), ''.join(candidates))
 print(m)
     
         
