8967708098358c5c2192aed8f7c42ac7d0829262
Ales Podolnik <ales.podolnik@gmail.com>
2018-12-23 18:26:43 +0100

    Day 12 slightly faster

--- 12/aoc12.py
+++ 12/aoc12.py
@@ -60,7 +60,8 @@ for i in range(1, total_n + 1):
         next_state[j] = Next(state, j, rules)
     state, offset = Wrap(next_state, offset)
     sum_n = SumPots(state, offset)
-    print(i, sum_n, sum_n / i)
+    if i % 100000 == 0:    
+        print(i, sum_n, sum_n / i)
 #    print(''.join([' '] * offset) + "0")
 #    print(''.join(state))
 sum_n = SumPots(state, offset)
